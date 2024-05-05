f575c72cdc508a5e04aed0af398400c3c5cd190c
Abie Safdie <abie@Abies-MacBook-Air.local>
2024-05-04 21:11:28 -0700

    collectors

--- scripts/distributed_security_monitoring.py
+++ scripts/distributed_security_monitoring.py
@@ -10,20 +10,23 @@ class BGPCollector:
         self.ip_prefix = ip_prefix
         self.collector = collector
         self.raw_announcement_data = []
+        self.raw_freq_data = []
         self.raw_withdrawal_data = []
 
     def collect_data(self, starttime=None):
 
-        url = f"https://stat.ripe.net/data/bgp-updates/data.json?resource={self.ip_prefix}" \
-              f"&rrcs={self.collector}&starttime=2024-04-15T17:59:51"
+        update_url = f"https://stat.ripe.net/data/bgp-updates/data.json?resource={self.ip_prefix}" \
+                     f"&rrcs={self.collector}&starttime=2024-04-01T17:59:51"
 
-        response = requests.get(url)
+        update_response = requests.get(update_url)
         bgp_updates = None
 
-        if response.status_code == 200:
-            data = response.json()
-            bgp_updates = data["data"]["updates"]
-
+        if update_response.status_code == 200:
+            update_data = update_response.json()
+            nr_updates = update_data["data"]["nr_updates"]
+            bgp_updates = update_data["data"]["updates"]
+            self.raw_freq_data.append(nr_updates)
+            path_lengths = []
             for update in bgp_updates:
                 type_of_update = update["type"]
                 if type_of_update == "A":
@@ -34,24 +37,28 @@ class BGPCollector:
                                                   "")
                     target_prefix = update["attrs"]["target_prefix"]
                     community = update["attrs"]["community"]
-                    self.raw_announcement_data.append([path_length, path, source_id, target_prefix, community])
-
+                    path_lengths.append(path_length)
+                    self.raw_announcement_data.append(
+                        [path, source_id, target_prefix, community])
                 else:
-                    print(update)
                     source_id = update["attrs"]["source_id"]
                     source_id = source_id.replace(f"{self.collector}-",
                                                   "")
                     target_prefix = update["attrs"]["target_prefix"]
 
                     self.raw_withdrawal_data.append([source_id, target_prefix])
+            self.raw_freq_data.append(path_lengths)
 
-
-    def print_raw_data(self, data):
+    def print_raw_data(self, data=None):
         if data == "A":
             print(self.raw_announcement_data)
-        else:
+        elif data == "W":
             print(self.raw_withdrawal_data)
+        else:
+            print(self.raw_freq_data)
 
+    def train_model(self):
+        pass
 
 
 class BGPCentral:
@@ -59,6 +66,11 @@ class BGPCentral:
 
 
 if __name__ == "__main__":
-    test = BGPCollector("159.121.0.0/16", "11")
-    test.collect_data()
-    # test.print_raw_data("W")
+    bgp_collector_11 = BGPCollector("159.121.0.0/16", "11")
+    bgp_collector_11.collect_data()
+
+    bgp_collector_12 = BGPCollector("159.121.0.0/16", "12")
+    bgp_collector_12.collect_data()
+
+    bgp_collector_14 = BGPCollector("159.121.0.0/16", "14")
+    bgp_collector_14.collect_data()
